ce4ff4a19152773281d65d13e01ca9d8990692b9
Yang Haibo <B40869@freescale.com>
2014-12-12 18:11:54 +0800

    add method: get node related obj from node id
    in our design, node is related to different type of objs
    we keep the parent-child relation in nodes,
    and keep more detail/type specific info in real objs
    
    It seems not a good idea....

--- fossil_app/db_util.py
+++ fossil_app/db_util.py
@@ -9,4 +9,8 @@ def add_node_type(name):
     db.session.commit()
 
 
-def add
+def get_node_obj(node_id):
+    sel_node = db.session.query(Node).get(node_id)
+    rel_class = globals()[sel_node.node_type.name]
+    rel_obj = rel_class.query.filter(rel_class.node_id==sel_node.id).one()
+    return rel_obj
